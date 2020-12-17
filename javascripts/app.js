a50c2591e835f2a4da873adb59a70a29cd17e3c5
Nicolas Sedano <23373@columbushs.com>
2020-12-17 09:20:49 -0500

    comit

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,67 @@
 // Rover Object Goes Here
 // ======================
-
+let rover = {
+  direction: "N",
+  x: 0,
+  y: 0,
+  travelLog: []
+}
 // ======================
 function turnLeft(rover){
   console.log("turnLeft was called!");
+}if (rover.direction === "N") {
+  rover.direction = "W";
+} else if (rover.direction === "W") {
+  rover.direction = "S";
+} else if (rover.direction === "S") {
+  rover.direction = "E";
+} else if (rover.direction === "E") {
+  rover.direction = "N"
 }
+
 
 function turnRight(rover){
   console.log("turnRight was called!");
 }
+if (rover.direction === "N") {
+  rover.direction = "E";
+} else if (rover.direction === "W") {
+  rover.direction = "N";
+} else if (rover.direction === "S") {
+  rover.direction = "W";
+} else if (rover.direction === "E") {
+  rover.direction = "S"
+}
+
 
 function moveForward(rover){
-  console.log("moveForward was called")
+  console.log("moveForward was called");
+
+if (rover.direction === "N"){
+  rover.y--;
+} else if (rover.direction === "S"){
+    rover.y++;
+} else if (rover.direction === "E"){
+  rover.x++;
+} else if (rover.direction === "W"){
+    rover.x--;
+  }
 }
+
+function commands (steps){
+  for(let i = 0; 1 < steps.length; i++){
+     console.log(steps.charAt(i))
+     let currentChar = steps.charAt(i)
+     if (currentChar === "l"){
+    turnLeft(rover);
+     } else if (currentChar === "r") {
+       turnRight(rover);
+     } else if (currentChar === "f") {
+      turnFo
+     }
+     rover.travelLog.push(currentChar);
+     }
+     console.log(rover.travelLog);
+    }
+
+
